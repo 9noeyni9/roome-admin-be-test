18091149415f1cd2d60056c9a2d1f1f26a390b88
9noeyni9 <koo949494@naver.com>
2025-05-14 00:42:29 +0900

    first commit

---
+++ src/main/java/com/roome/admin/roomeadminbe/RoomeAdminBeApplication.java
@@ -0,0 +1,13 @@
+package com.roome.admin.roomeadminbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomeAdminBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomeAdminBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roome/admin/roomeadminbe/RoomeAdminBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roome.admin.roomeadminbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomeAdminBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
