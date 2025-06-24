4990492472f4e6f667fd55b41b166d4d723d4dc7
inyeong <iykoo@dsmentoring.com>
2025-06-24 14:34:02 +0900

    fix: 불필요한 주석 코드 삭제

--- src/main/java/com/roome/admin/roomeadminbe/domain/auth/service/AuthService.java
+++ src/main/java/com/roome/admin/roomeadminbe/domain/auth/service/AuthService.java
@@ -1,11 +1,7 @@
 package com.roome.admin.roomeadminbe.domain.auth.service;
 
-import com.roome.admin.roomeadminbe.domain.admin.entity.Admin;
-import com.roome.admin.roomeadminbe.domain.admin.repository.AdminRepository;
 import com.roome.admin.roomeadminbe.domain.auth.dto.TokenResponseDto;
 import com.roome.admin.roomeadminbe.domain.auth.dto.request.LoginRequest;
-import com.roome.admin.roomeadminbe.domain.auth.dto.request.SendTempPasswordRequest;
-import com.roome.admin.roomeadminbe.global.mail.MailService;
 import com.roome.admin.roomeadminbe.global.security.jwt.provider.TokenProvider;
 import com.roome.admin.roomeadminbe.global.security.jwt.service.RefreshTokenService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -16,7 +12,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.security.SecureRandom;
@@ -29,27 +24,9 @@ import static com.roome.admin.roomeadminbe.global.security.util.CookieUtil.delet
 @RequiredArgsConstructor
 public class AuthService {
 
-	private final AdminRepository adminRepository;
-	private final MailService mailService;
-	private final PasswordEncoder passwordEncoder;
 	private final TokenProvider tokenProvider;
 	private final AuthenticationManagerBuilder authenticationManagerBuilder;
 	private final RefreshTokenService refreshTokenService;
-
-//	public void sendTempPassword(SendTempPasswordRequest sendTempPasswordRequest) {
-//		Admin admin = adminRepository.findByAdminEmail(sendTempPasswordRequest.getAdminEmail())
-//				.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 관리자입니다."));
-//
-//		// 1. 임시 비밀번호 생성
-//		String tempPassword = generateRandomPassword();
-//
-//		// 2. 비밀번호 저장
-//		admin.updateTempPassword(passwordEncoder.encode(tempPassword));
-//		adminRepository.save(admin);
-//
-//		// 3. 메일 전송 요청
-//		mailService.sendTempPasswordEmail(admin.getAdminEmail(), tempPassword);
-//	}
 
 	public TokenResponseDto login(LoginRequest loginRequestDto) {
 		UsernamePasswordAuthenticationToken authenticationToken =
