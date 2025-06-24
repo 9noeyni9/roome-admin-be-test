a00d54e34b3a887c76687ded32a0632619f9c0c5
inyeong <iykoo@dsmentoring.com>
2025-06-24 15:59:44 +0900

    fix: 응답 Void 로 변경(AdminController)

--- src/main/java/com/roome/admin/roomeadminbe/domain/admin/controller/AdminController.java
+++ src/main/java/com/roome/admin/roomeadminbe/domain/admin/controller/AdminController.java
@@ -30,14 +30,14 @@ public class AdminController {
 	}	
 
 	@PatchMapping
-	public ResponseEntity<CommonResponse<String>> updateInfo(@AuthenticationPrincipal UserDetails userDetails, @RequestBody @Validated UpdateAdminInfoRequest updateAdminInfoRequest) {
+	public ResponseEntity<CommonResponse<Void>> updateInfo(@AuthenticationPrincipal UserDetails userDetails, @RequestBody @Validated UpdateAdminInfoRequest updateAdminInfoRequest) {
 		adminService.updateInfo(userDetails.getUsername(), updateAdminInfoRequest);
-		return ofDataWithHttpStatus("사용자 정보 변경 완료", HttpStatus.OK);
+		return ofDataWithHttpStatus(null, HttpStatus.OK);
 	}
 
 	@PutMapping("/password")
-	public ResponseEntity<CommonResponse<String>> updatePassword(@AuthenticationPrincipal UserDetails userDetails, @RequestBody @Validated UpdatePasswordRequest updatePasswordRequest) {
+	public ResponseEntity<CommonResponse<Void>> updatePassword(@AuthenticationPrincipal UserDetails userDetails, @RequestBody @Validated UpdatePasswordRequest updatePasswordRequest) {
 		adminService.updatePassword(userDetails.getUsername(), updatePasswordRequest);
-		return ofDataWithHttpStatus("비밀번호 변경 완료", HttpStatus.OK);
+		return ofDataWithHttpStatus(null, HttpStatus.OK);
 	}
 }
